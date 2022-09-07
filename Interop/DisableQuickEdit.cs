9f0fb3e6f88804b81bdc9dff83a5d034cf3cb343
JohnyWander <j.myslinski.pl@gmail.com>
2022-09-07 11:27:21 +0200

    x

---
+++ Interop/DisableQuickEdit.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssh_mon.Interop
+{
+    internal static class _DisableQuickEdit_
+    {
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool GetConsoleMode(
+            IntPtr hConsoleHandle,
+            out int lpMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetConsoleMode(
+            IntPtr hConsoleHandle,
+            int ioMode);
+
+        public const int STD_INPUT_HANDLE = -10;
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetStdHandle(int nStdHandle);
+
+        /// <summary>
+        /// This flag enables the user to use the mouse to select and edit text. To enable
+        /// this option, you must also set the ExtendedFlags flag.
+        /// </summary>
+        const int QuickEditMode = 64;
+
+        // ExtendedFlags must be combined with
+        // InsertMode and QuickEditMode when setting
+        /// <summary>
+        /// ExtendedFlags must be enabled in order to enable InsertMode or QuickEditMode.
+        /// </summary>
+        const int ExtendedFlags = 128;
+
+       public static void DisableQuickEdit()
+        {
+            IntPtr conHandle = GetStdHandle(STD_INPUT_HANDLE);
+            int mode;
+
+            if (!GetConsoleMode(conHandle, out mode))
+            {
+                // error getting the console mode. Exit.
+                return;
+            }
+
+            mode = mode & ~(QuickEditMode | ExtendedFlags);
+
+            if (!SetConsoleMode(conHandle, mode))
+            {
+                // error setting console mode.
+            }
+        }
+        public static void EnableQuickEdit()
+        {
+
+            IntPtr conHandle = GetStdHandle(STD_INPUT_HANDLE);
+            int mode;
+
+            if (!GetConsoleMode(conHandle, out mode))
+            {
+                // error getting the console mode. Exit.
+                return;
+            }
+
+            mode = mode | (QuickEditMode | ExtendedFlags);
+
+            if (!SetConsoleMode(conHandle, mode))
+            {
+                // error setting console mode.
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
